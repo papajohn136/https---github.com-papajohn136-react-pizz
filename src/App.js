47ab7560c799aec648c1dcdc12e5213fd4d56e51
Evgeniy Borisov <e.borisov99@gmail.com>
2024-03-03 22:41:39 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import '../src/src/scss/app.scss';
+import Categories from './components/Categories';
+import Header from './components/Header';
+import PizzaBlock from './components/PizzaBlock';
+import Sort from './components/Sort';
+import book from './img/book.png'
+import book2 from './img/book2.png'
+import book3 from './img/book3.png'
+import book4 from './img/book4.png'
+
+function App() {
+  return (
+    <div className ="wrapper">
+      <Header />
+      <div className ="content">
+        <div className ="container">
+          <div className ="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className ="content__title">Все книги</h2>
+          <div className ="content__items">
+            <PizzaBlock 
+            title = "Властелин Колец Трилогия"
+            price = {5000}
+            src = {book}
+            />
+            <PizzaBlock 
+            title = "Властелин Колец Трилогия"
+            price = {6000}
+            src = {book2}
+            />
+            <PizzaBlock 
+            title = "Властелин Колец Трилогия"
+            price = {5000}
+            src = {book3}/>
+            <PizzaBlock 
+            title = "Бестиарий Ведьмак"
+            price = {3500}
+            src = {book4}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Categories.jsx
@@ -0,0 +1,17 @@
+
+function Categories() {
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">Все</li>
+        <li>Властелин колец</li>
+        <li>Ведьмак</li>
+        <li>Метро Исход</li>
+        <li>Bloodborne</li>
+        <li>Закрытые</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories
